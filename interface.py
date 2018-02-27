ce7e23a67b3aadc17f1e287f42e91b70309a6001
Federico Zarelli <Federico>
2018-02-27 14:51:38 +0100

    added method for get valves and pipes

--- interface.py
+++ interface.py
@@ -8,6 +8,7 @@ class interface(object):
         self.board = self.building.switch_board_list[board_ID]   # good practice to short names
         self.sensors_lists = {}  # redundant definition for intelligibility
         self.pumps_list = {}  # redundant definition for intelligibility
+        self.pipes_list = {}
 
     def get_hydraulic_bay(self, switch_board, hydraulic_bay_ID):
         return switch_board.bays_list[hydraulic_bay_ID]
@@ -36,6 +37,8 @@ class interface(object):
         for key in self.sensors_lists.keys():
             print(self.sensors_lists[key].get_name())
 
+        return self.pumps_list
+
     def get_system_pumps(self):
         bays_list = self.board.bays_list  # ridefinition of variable for clarity
 
@@ -58,3 +61,44 @@ class interface(object):
         self.pumps_list = line_pumps_list
         for key in self.pumps_list.keys():
             print(self.pumps_list[key].get_name())
+
+        return self.pumps_list
+
+    def get_system_valves(self):
+        bays_list = self.board.bays_list  # ridefinition of variable for clarity
+
+        bay_valves_list = {}
+        for bay in bays_list.keys():  # parsing dictionary keys
+            print(bay)
+            hydraulic_bay = bays_list[bay]
+            bay_valves_list.update(hydraulic_bay.valves_list)
+
+        self.bay_valves_list = bay_valves_list
+        for key in self.bay_valves_list.keys():
+            print(self.bay_valves_list[key].get_name())
+
+        return self.bay_valves_list
+
+    def get_system_pipes(self):
+        bays_list = self.board.bays_list  # ridefinition of variable for clarity
+
+        pipes_in_list = {}   # ridefinition of variable for clarity
+        pipes_out_list = {}
+
+        for bay in bays_list.keys():  # parsing dictionary keys
+            print(bay)
+            hydraulic_bay = bays_list[bay]
+            pipes_in_list.update(hydraulic_bay.pipes_in_list)
+            pipes_out_list.update(hydraulic_bay.pipes_out_list)
+
+        self.pipes_in_list = pipes_in_list
+        self.pipes_out_list = pipes_out_list
+        self.pipes_list["in"] = self.pipes_in_list
+        self.pipes_list["out"] = self.pipes_out_list
+
+        for key in self.pipes_in_list.keys():
+            print(self.pipes_in_list[key].get_name())
+        for key in self.pipes_out_list.keys():
+            print(self.pipes_out_list[key].get_name())
+
+        return self.pipes_list
