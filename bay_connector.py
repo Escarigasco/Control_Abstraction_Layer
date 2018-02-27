fd4183ee093267b147098efbb48da0b7336c6bc5
Federico Zarelli <Federico>
2018-02-27 12:24:00 +0100

    renamed dictionary

--- bay_connector.py
+++ bay_connector.py
@@ -8,10 +8,10 @@ class bay_connector:
     def __init__(self, bay_ID, ID, connector, output_line):
         self.bay_ID = bay_ID
         self.ID = ID
-        self.line_list = {}
+        self.lines_list = {}
         self.connector = connector
         self.output_line = output_line
-        self.connector_sensor_list = {}
+        self.connector_sensors_list = {}
 
         lines = self.connector.find_all("line")
         sensors = self.connector.find_all("sensor", position="connector")
@@ -19,14 +19,14 @@ class bay_connector:
         for line in lines:
             if (self.output_line == line["type"]):
                 is_output = True
-                self.line_list[line["id"]] = connector_line(self.ID, line["id"], line, line["type"], is_output)
+                self.lines_list[line["id"]] = connector_line(self.ID, line["id"], line, line["type"], is_output)
 
             else:
                 is_output = False
-                self.line_list[line["id"]] = connector_line(self.ID, line["id"], line, line["type"], is_output)
+                self.lines_list[line["id"]] = connector_line(self.ID, line["id"], line, line["type"], is_output)
 
         for sensor in sensors:
-            self.connector_sensor_list[sensor["id"]] = system_sensor(self.ID, sensor["id"], sensor["variable"], sensor["embedded"], sensor["position"])  # don't pass the soup as is the end of the tree'''
+            self.connector_sensors_list[sensor["id"]] = system_sensor(self.ID, sensor["id"], sensor["variable"], sensor["embedded"], sensor["position"])  # don't pass the soup as is the end of the tree'''
 
     def get_name(self):
         return self.ID
