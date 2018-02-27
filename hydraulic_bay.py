c387fbdd6f8d6b5b91395b27b4bb42c90d0037da
Federico Zarelli <Federico>
2018-02-27 10:09:50 +0100

    renamded return_line in output_line

--- hydraulic_bay.py
+++ hydraulic_bay.py
@@ -1,5 +1,4 @@
 # Bay Class
-from bs4 import BeautifulSoup as Soup
 from pipeline import pipeline
 from bay_valve import bay_valve
 from bay_connector import bay_connector
@@ -13,28 +12,28 @@ class hydraulic_bay(object):
         self.ID = ID
         self.pipes_in_list = {}
         self.pipes_out_list = {}
-        self.valves = {}
+        self.valves_list = {}
         self.bay = bay
+        self.connector_list = {}
 
         connector = self.bay.find("connector")  # find connectors
         valves = self.bay.find_all("valve")     # find valves
 
         if (self.bay["connected_device"] == "source"):
-            return_line = "H"  # discrimination for the connector
+            output_line = "H"  # discrimination for the connector
             pipes_in = self.bay.find_all("pipe", busbar=["1C", "2C", "B"])   # find inlet pipes
             pipes_out = self.bay.find_all("pipe", busbar=["1H", "2H", "B"])  # find outlet pipes
-            self.connector_list[connector["id"]] = bay_connector(self.ID, connector["id"], connector, return_line)  # create connectors object
+            self.connector_list[connector["id"]] = bay_connector(self.ID, connector["id"], connector, output_line)  # create connectors object
 
         elif (bay["connected_device"] == "sink"):
-            return_line = "C"  # discrimination for the connector
+            output_line = "C"  # discrimination for the connector
             pipes_in = self.bay.find_all("pipe", busbar=["1H", "2H", "B"])   # find inlet pipes
             pipes_out = self.bay.find_all("pipe", busbar=["1C", "2C", "B"])  # find inlet pipes
-            self.connector_list[connector["id"]] = bay_connector(self.ID, connector["id"], connector, return_line)  # create connectors object
+            self.connector_list[connector["id"]] = bay_connector(self.ID, connector["id"], connector, output_line)  # create connectors object
 
         for pipe in pipes_in:
             direction = "in"
             self.pipes_in_list[pipe["id"]] = pipeline(self.ID, pipe["id"], direction)  # creates pipes
-            print(pipe)
 
         for pipe in pipes_out:
             direction = "out"
