0e744190794fd8443d88f4e28de7c6eae42dcc93
Federico Zarelli <Federico>
2018-02-27 16:41:10 +0100

    inserted object tracker and collection of arrays from intf

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 from switch_board_building import switch_board_building
 from interface import interface
+from object_tracker import object_tracker
 #  from IPython.core.debugger import Tracer
 #  Tracer()()
 
@@ -8,14 +9,23 @@ from interface import interface
 BuildingID = "Building716"
 Receiver = "First"
 SwitchID = "Switch_Board_1"
-
+Parameter = "E"
+Sensor = "Sensor_1E1"
+Setpoint = "5"
+Source = "Source_1HP1"
 
 building716 = switch_board_building(BuildingID)
 
 intf = interface(building716, SwitchID)
-
+objtk = object_tracker(intf)
 system_sensors = intf.get_system_sensors()
 system_pumps = intf.get_system_pumps()
+system_valves = intf.get_system_valves()
+system_pipes = intf.get_system_pipes()
+sensors_position = objtk.where_are_sensors(system_sensors)
+
+print(sensors_position.keys())
+print(sensors_position.values())
 
 
 # server = input_receiver()
