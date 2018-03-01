2d25f21306030247645aadf74f44fd67e8f32644
Federico Zarelli <Federico>
2018-03-01 15:31:57 +0100

    inserted variables and dict related to the valve object creation

--- pipeline.py
+++ pipeline.py
@@ -1,13 +1,21 @@
 # Pipeline Class
+from bay_valve import bay_valve
 
 
 class pipeline(object):
     'Class for Pipeline definition and properties'
 
-    def __init__(self, bay_ID, ID, direction):
+    def __init__(self, bay_ID, ID, pipe, direction):
         self.bay_ID = bay_ID
         self.ID = ID
+        self.pipe = pipe
         self.direction = direction
+        self.valves_list = {}
+
+        valves = self.pipe.find_all("valve")     # find valves
+
+        for valve in valves:
+            self.valves_list[valve["id"]] = bay_valve(self.ID, valve["id"], valve["connection"], valve["flow"])  # creates valves
 
     def get_name(self):
         return self.ID
