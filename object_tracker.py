4061c4bd5790d74bc18361922b67907c2def6b4e
Federico Zarelli <Federico>
2018-03-06 12:46:54 +0100

    removed busbar method

--- object_tracker.py
+++ object_tracker.py
@@ -11,11 +11,10 @@ class object_tracker(object):
 
     def where_are_sensors(self, sensors):
         final_location = "Bay_"
-        sensors_list = sensors
         parents_list = self.intf.get_all_parents()
 
-        for key in sensors_list.keys():
-            sensor = sensors_list[key]
+        for key in sensors.keys():
+            sensor = sensors[key]
             sensor_location = sensor.get_parent()
 
             while not (re.match(final_location, sensor_location)):
@@ -30,11 +29,10 @@ class object_tracker(object):
 
     def where_are_connected_devices(self, devices):
         final_location = "Bay_"
-        device_list = devices
         parents_list = self.intf.get_all_parents()
 
-        for key in device_list.keys():
-            device = device_list[key]
+        for key in devices.keys():
+            device = devices[key]
             device_location = device.get_parent()
 
             while not (re.match(final_location, device_location)):
