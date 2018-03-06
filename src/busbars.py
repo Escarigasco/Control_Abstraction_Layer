ff6a75f250d055c92655b03bf0c4755a88621477
Federico Zarelli <Federico>
2018-03-06 17:56:06 +0100

    add method get status

--- src/busbars.py
+++ src/busbars.py
@@ -6,7 +6,11 @@ class busbar(object):
     def __init__(self, circuit_ID, system_pipes, status):
         self.system_pipes = system_pipes
         self.circuit_ID = circuit_ID
-        self.system_pipes = system_pipes
+        self.status = status
+        print(system_pipes)
+
+    def get_status():
+        return self.status
 
     def get_pipes(self):
         return self.system_pipes
