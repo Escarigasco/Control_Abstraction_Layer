a5891de3d0a6fccd4c5c6c1bf3ac8815c7367007
Federico Zarelli <Federico>
2018-03-06 14:41:31 +0100

    busbar object

---
+++ busbars.py
@@ -0,0 +1,21 @@
+# Class for busbars
+
+
+class busbar(object):
+
+    def __init__(self, circuit_ID, system_pipes, status):
+        self.system_pipes = system_pipes
+        self.circuit_ID = circuit_ID
+        self.system_pipes = system_pipes
+
+    def get_pipes(self):
+        return self.system_pipes
+
+    def get_name(self):
+        return self.circuit_ID
+
+    def __repr__(self):
+        return "<BusBar, id: {0}>".format(self.circuit_ID)
+
+    def __str__(self):
+        return "<BusBar, id: {0}>".format(self.circuit_ID)
