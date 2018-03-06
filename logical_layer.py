ceca30953e6baf80915ebc97949ffd38de4b4e45
Federico Zarelli <Federico>
2018-03-06 12:49:03 +0100

    moved intefaces get method in the initilization

--- logical_layer.py
+++ logical_layer.py
@@ -1,6 +1,7 @@
 from switch_board_building import switch_board_building
 from interface import interface
 from object_tracker import object_tracker
+
 #  from IPython.core.debugger import Tracer
 #  Tracer()()
 
@@ -13,11 +14,16 @@ class logical_layer(object):
         self.Receiver = "First"
         self.SwitchID = "Switch_Board_1"
 
+        self.building716 = switch_board_building(self.BuildingID)
+        self.intf = interface(self.building716, self.SwitchID)
 
-        Parameter = "E"
-        Sensor = "Sensor_1E1"
-        Setpoint = "5"
-        Source = "Source_1HP1"
+        self.system_sensors = self.intf.get_system_sensors()
+        self.system_pumps = self.intf.get_system_pumps()
+        self.system_valves = self.intf.get_system_valves()
+        self.system_pipes = self.intf.get_system_pipes()
+        self.system_connected_devices = self.intf.get_connected_devices()
+        self.system_bays = self.intf.get_hydraulic_bays()
+        self.system_busbars = self.intf.build_busbars(self.system_pipes)
 
     def run(self, sensors, parameters, setpoints, sources):
 
@@ -25,37 +31,26 @@ class logical_layer(object):
         self.parameters_list = parameters
         self.setpoints_list = setpoints
         self.sources_list = sources
-        print(self.sensors_list.keys())
-        print(self.sensors_list.values())
 
-        building716 = switch_board_building(self.BuildingID)
-        intf = interface(building716, self.SwitchID)
-        objtk = object_tracker(intf)
+        objtk = object_tracker(self.intf)
 
-        system_sensors = intf.get_system_sensors()
-        system_pumps = intf.get_system_pumps()
-        system_valves = intf.get_system_valves()
-        system_pipes = intf.get_system_pipes()
-        system_connected_devices = intf.get_connected_devices()
-        system_bays = intf.get_hydraulic_bays()
-
-        sensors_position = objtk.where_are_sensors(system_sensors)
-        connected_device_position = objtk.where_are_connected_devices(system_connected_devices)
+        sensors_position = objtk.where_are_sensors(self.system_sensors)
+        connected_device_position = objtk.where_are_connected_devices(self.system_connected_devices)
 
         # computational_unit = path_calculator() - it will define all the pipes entering the bays that
-        # print(sensors_position.keys())
-        # print(sensors_position.values())
-        # print(connected_device_position.keys())
-        # print(connected_device_position.values())
+        print(sensors_position.keys())
+        print(sensors_position.values())
+        print(connected_device_position.keys())
+        print(connected_device_position.values())
 
-        # return networkx object
+    # return networkx object
 
-        # server = input_receiver()
+    # server = input_receiver()
 
-        # user_inputs =  Server.get_commands()
+    # user_inputs =  Server.get_commands()
 
-        # classe = building716.interface(user_inputs)
+    # classe = building716.interface(user_inputs)
 
-        # components_involved = computational_unit.get_path(classe, user_inputs)
+    # components_involved = computational_unit.get_path(classe, user_inputs)
 
     # components_functions = computation_unit.get_functions(components_involved, classe, user_inputs)
