e899c7e6ea34f00ae83a27ede3228c17b161dc13
Federico Zarelli <Federico>
2018-03-20 18:17:19 +0100

    just a test

--- src/rule_engine.py
+++ src/rule_engine.py
@@ -1,14 +1,38 @@
 from pyknow import *
+import networkx as nx
+from networkx.algorithms import isomorphism
+from matplotlib import pyplot as plt
+
 
 class Light(Fact):
     """Info about the traffic light."""
     pass
 
 
+class Busbar(Fact):
+    """Info about the traffic light."""
+    pass
+
+
+class Valve(Fact):
+    """Info about the traffic light."""
+    pass
+
+
 class RobotCrossStreet(KnowledgeEngine):
-    def __init__(self, test):
-        self.obj = test
+
+    def __init__(self, graph):
+        self.graph = graph
         super().__init__()
+
+    @Rule(Busbar(busbar='device' << W(), ID='1H'),
+          #Busbar(ID='1H'),
+          Valve(valve='valve' << W(), connection='1H', status=1))
+    def select_busbar(self, device, valve):
+        self.graph.add_edges_from([(device.get_name(), valve.get_name())])
+
+
+
 
     @Rule(Light(variable='test_object' << W()), salience=1)
     def test_name(self, test_object):
@@ -19,7 +43,7 @@ class RobotCrossStreet(KnowledgeEngine):
     @Rule(Light(color='green'))
     def green_light(self):
         print("Walk")
-        self.obj.set_colour("red")
+
 
     @Rule(Light(color='red'))
     def red_light(self):
@@ -52,16 +76,43 @@ class shirt(object):
 
 class rule_engine(object):
 
-    def __init__(self, system_sensors, system_pumps, system_valves, system_connected_devices, system_bays, system_busbars, system_input, sensors_position, lines_valve_connection):
+    def __init__(self, interface):
         print("I am here")
+        self.interface = interface
+
+    def run(self):
+        Graph = nx.DiGraph()
+        engine = RobotCrossStreet(Graph)
+        engine.reset()
+        system_bays = self.interface.get_hydraulic_bays()
+        system_pumps = self.interface.get_system_pumps()
+        system_sensors = self.interface.get_system_sensors()
+        system_valves = self.interface.get_system_valves()
+        system_connectors = self.interface.get_system_connectors()
+        system_lines = self.interface.get_system_lines()
+        system_pipes = self.interface.get_system_pipes()
+        system_connected_devices = self.interface.get_connected_devices()
+        system_bays = self.interface.get_hydraulic_bays()
+        system_busbars = self.interface.build_busbars(system_pipes)
+
+        for busbar in system_busbars.keys():
+            engine.declare(Busbar(busbar=system_busbars[busbar], ID=system_busbars[busbar].get_name()))
+            Graph.add_node(busbar)
+        for valve in system_valves.keys():
+            engine.declare(Valve(valve=system_valves[valve], connection=system_valves[valve].get_connection(), status=system_valves[valve].get_status()))
+            Graph.add_node(valve)
+
+
         newshirt = shirt()
         oldshirt = shirt()
         oldshirt.set_colour("lilla")
-        engine = RobotCrossStreet(newshirt)
-        engine.reset()
+
+
         color = 'green'
         engine.declare(Light(color=color))
         engine.declare(Light(variable=newshirt))
         engine.declare(Light(variable=oldshirt))
-        print(engine.facts)
+
         engine.run()
+        nx.draw(Graph, with_labels=True)
+        plt.show()
