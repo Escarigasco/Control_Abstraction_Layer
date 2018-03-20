106657056e2a2e29829e424a753e14a71c964b58
Federico Zarelli <Federico>
2018-03-20 18:09:56 +0100

    added parameter for status and flow

--- src/bay_valve.py
+++ src/bay_valve.py
@@ -4,11 +4,17 @@
 class bay_valve:
     'Class for Valve definition and properties'
 
-    def __init__(self, parent_ID, ID, connection, flow):
+    def __init__(self, parent_ID, ID, connection, flow, direction):
         self.parent_ID = parent_ID
         self.ID = ID
         self.connection = connection
         self.flow = flow
+        self.flow_direction = direction
+        print(self.ID)
+        print(self.flow_direction)
+
+    def get_flow_direction(self):
+        return self.flow_direction
 
     def get_name(self):
         return self.ID
@@ -16,14 +22,23 @@ class bay_valve:
     def get_flow(self):
         return self.flow
 
+    def get_connection(self):
+        return self.connection
+
     def get_parent(self):
         return self.parent_ID
 
-    def get_position(self):
-        return self.position  # this needs to be real time read
+    def set_status(self, status):
+        if (status > 20):
+            self.status = 1
+        else:
+            self.status = 0
+
+    def get_status(self):
+        return self.status
 
     def __repr__(self):
-        return "<Valve, id: {0}>".format(self.ID)
+        return "id: {0}".format(self.ID)
 
     def __str__(self):
-        return "<Valve, id: {0}>".format(self.ID)
+        return "id: {0}".format(self.ID)
