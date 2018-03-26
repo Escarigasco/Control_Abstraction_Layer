13031d7150235909a1fe8553779eb523076d15e9
Federico Zarelli <Federico>
2018-03-26 16:09:23 +0200

    first commit

---
+++ src/path_matcher.py
@@ -0,0 +1,6 @@
+
+
+class path_matcher(object):
+    def __init__(online_configuration, possible_configurations):
+        self.online_configuration = online_configuration
+        self.possible_configurations = possible_configurations
