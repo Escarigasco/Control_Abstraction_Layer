541b00c2e1a0e5e6df5f6da90d3d3c31884000cc
Federico Zarelli <Federico>
2018-03-26 16:03:32 +0200

    introduced type and method to set type and flow_type

--- src/busbars.py
+++ src/busbars.py
@@ -1,15 +1,19 @@
 # Class for busbars
+_FIRST_OF_THE_CLASS = 0
 
 
 class busbar(object):
 
-    def __init__(self, circuit_ID, system_pipes, status):
+    def __init__(self, circuit_ID, system_pipes):
         self.system_pipes = system_pipes
         self.circuit_ID = circuit_ID
-        self.status = status
+        self.set_flow_and_type()
 
-    def get_status():
-        return self.status
+    def set_flow_and_type(self):
+        self.type = self.system_pipes[_FIRST_OF_THE_CLASS].type
+        valve_name = self.system_pipes[_FIRST_OF_THE_CLASS].valves_list.keys()
+        for name in valve_name:
+            self.flow = self.system_pipes[_FIRST_OF_THE_CLASS].valves_list[name].get_flow()
 
     def get_pipes(self):
         return self.system_pipes
