567e16626238b27b456a6c233dfbadbaaeb54aa8
Federico Zarelli <Federico>
2018-03-26 16:08:00 +0200

    added parameter for type

--- src/pipeline.py
+++ src/pipeline.py
@@ -5,22 +5,25 @@ from bay_valve import bay_valve
 class pipeline(object):
     'Class for Pipeline definition and properties'
 
-    def __init__(self, parent_ID, ID, pipe, busbar, direction):
+    def __init__(self, parent_ID, ID, pipe, busbar, type, direction):
         self.parent_ID = parent_ID
         self.ID = ID
         self.pipe = pipe
         self.direction = direction
         self.valves_list = {}
         self.busbar = busbar
+        self.type = type
 
         valves = self.pipe.find_all("valve")     # find valves
 
         for valve in valves:
             self.valves_list[valve["id"]] = bay_valve(self.ID, valve["id"], valve["connection"], valve["flow"], self.direction)  # creates valves
 
-
     def get_parent(self):
         return self.parent_ID
+
+    def get_type(self):
+        return self.type
 
     def get_name(self):
         return self.ID
