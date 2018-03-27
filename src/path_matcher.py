fbe9d82807adfbd588aecf469fa82bcb1603ca09
Federico Zarelli <Federico>
2018-03-27 11:47:32 +0200

    tested and operating matching logic - to be corrected sintax and return value

--- src/path_matcher.py
+++ src/path_matcher.py
@@ -13,9 +13,58 @@ class path_matcher(object):
         self.possible_configurations = possible_configurations
 
     def run(self):
-        a = 0
-        for possible_config in self.possible_configurations:
-            DiGM = isomorphism.GraphMatcher(self.online_configuration, possible_config)
-            is_good = DiGM.subgraph_is_isomorphic()
-            print(is_good)
-            print(DiGM)
+
+
+
+        print("I start computing")
+
+        for n in self.possible_configurations[1]:
+            print(n)
+        #print(self.possible_configurations[0].edges)
+        #print(nx.compose(self.possible_configurations[0], self.online_configuration))
+
+        G = self.online_configuration.subgraph(self.possible_configurations[0].nodes)
+        plt.figure(6)
+        nx.draw_circular(G, font_size=8, node_size=40, alpha=0.5, node_color="blue", with_labels=True)
+
+
+        H = self.online_configuration.edge_subgraph(self.possible_configurations[1].edges)
+        plt.figure(7)
+        nx.draw_circular(H, font_size=8, node_size=40, alpha=0.5, node_color="blue", with_labels=True)
+
+        I = self.online_configuration.subgraph(self.possible_configurations[2].nodes)
+        plt.figure(8)
+        nx.draw_circular(I, font_size=8, node_size=40, alpha=0.5, node_color="blue", with_labels=True)
+
+
+        L = self.online_configuration.edge_subgraph(self.possible_configurations[3].edges)
+        plt.figure(9)
+        nx.draw_circular(L, font_size=8, node_size=40, alpha=0.5, node_color="blue", with_labels=True)
+
+
+        print("I start computing")
+        DiGM = isomorphism.GraphMatcher(G, self.possible_configurations[0])
+        print("done with inizialization")
+        is_good = DiGM.subgraph_is_isomorphic()
+        print(is_good)
+
+        print("I start computing")
+        DiGM = isomorphism.GraphMatcher(H, self.possible_configurations[1])
+        print("done with inizialization")
+        is_good = DiGM.subgraph_is_isomorphic()
+        print(is_good)
+
+
+        print("I start computing")
+        DiGM = isomorphism.GraphMatcher(I, self.possible_configurations[2])
+        print("done with inizialization")
+        is_good = DiGM.subgraph_is_isomorphic()
+        print(is_good)
+
+        print("I start computing")
+        DiGM = isomorphism.GraphMatcher(L, self.possible_configurations[3])
+        print("done with inizialization")
+        is_good = DiGM.subgraph_is_isomorphic()
+        print(is_good)
+
+        plt.show()
