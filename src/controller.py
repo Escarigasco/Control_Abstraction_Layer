b2f2ab343ece489a1b645ff0e63bb5af19830293
Federico Zarelli <Federico>
2018-05-07 11:10:11 +0200

    introduced error handling, ID and tuple as controller argument

--- src/controller.py
+++ src/controller.py
@@ -1,42 +1,64 @@
-import _thread
 # import SWITCHBOARD_PYTHON_API
 # remember that additional gain can be added by multiplying the error by a constant
 # you could initialize this controller as a class when you initialize the logical layer and then call the method that start the thread every time you need it
+import _thread
+import pickle
+import sys
+import time
 
 
 class controller(object):
 
     def read_feedback():
-
+        value = 0
         return value
 
-    def PID_controller(inputs):
+    def PID_controller(self, inputs, thread_ID):
+        print("Control Thread {0} started".format(thread_ID))
+        print(thread_ID)
+        inputs = pickle.loads(inputs)
+        print(inputs)
         error = []
         time_response = []
-        kp = inputs["Kp"]
-        kd = inputs["Ki"]
-        ki = inputs["Kd"]
-        gain = inputs["Gain"]
+        kp = inputs["kp"]
+        kd = inputs["ki"]
+        ki = inputs["kd"]
+        gain = inputs["gain"]
         circulators = inputs["circulator"]
-        circulator_mode = inputs["Circulator_mode"]
+        circulator_mode = inputs["circulator_mode"]
+        integral = 0
+        pre_error = 0
         # INTERFACE = SWITCHBOARD_PYTHON_API()
-        for n in len(circulators):
-            print(n)
+        #for n in len(circulators):
+            #print(n)
             # INTERFACE.SET_CIRCULATOR_MODE(circulators[n], circulator_mode[n])
         while(1):
-            feedback_value =  # INTERFACE.read_feedback("SENSOR")                    # Get the feedback value
-            setpoint = inputs["Setpoint"]                         # Get the set value
+            try:
+                print("Control Thread {0} running".format(thread_ID))
+                time.sleep((thread_ID + 2))
+                feedback_value = 5  # INTERFACE.read_feedback("SENSOR")                    # Get the feedback value
+                setpoint = inputs["setpoint"]                         # Get the set value
 
-            error_value = gain * (setpoint - feedback_value)       # Calculate the error
-            integral = integral + error_value              # Calculate integral
-            derivative = error_value - pre_error           # Calculate derivative
+                error_value = gain * (setpoint - feedback_value)       # Calculate the error
+                integral = integral + error_value              # Calculate integral
+                derivative = error_value - pre_error           # Calculate derivative
 
-            output = (kp * error_value) + (ki * integral) + (kd * derivative)  # Calculate the output, pwm.
+                output = (kp * error_value) + (ki * integral) + (kd * derivative)  # Calculate the output, pwm.
 
-            if (output > 100):
-                output = 100  # Limit the output to maximum 255.
-            elif (output < 1):
-                output = 1
+                if (output > 100):
+                    output = 100  # Limit the output to maximum 255.
+                elif (output < 1):
+                    output = 1
 
-            error.append(error_value)
-            time_response.append(feedback_value)  # Save as previous error.
+                error.append(error_value)
+                time_response.append(feedback_value)  # Save as previous error.
+            except (KeyboardInterrupt, SystemExit, Exception):
+                print("Thread Stopped")
+                _thread.exit()
+                sys.exit()
+
+
+if __name__ == "__main__":
+    test = controller()
+    input_for_controller = {"gain": 1, "kp": 2.58, "ki": 2.58, "kd": 0, "circulator": "Pump", "circulator_mode": "constant m", "actuator": "pump", "setpoint": 50, "feedback": 'sensors'}
+    test.PID_controller(input_for_controller)
--- src/physical_layer.py
+++ src/physical_layer.py
@@ -1,27 +1,50 @@
 # Create two threads as follow
 import _thread
 from controller import controller
-import pickle
 import socket
+import sys
+# import pickle
+
 
 HOST = 'localhost'                 # Symbolic name meaning all available interfaces
 PORT = 50008              # Arbitrary non-privileged port
 op_controller = controller()
+n = 0
+threads = []
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    # op_controller = controller()
     s.bind((HOST, PORT))
     while True:
+        print("Physical Layer Listening")
         s.listen(1)
         conn, addr = s.accept()
         with conn:
-            print('Connected by', addr)
-            while True:
-                data = conn.recv(1024)
-                input_for_controller = pickle.loads(data)
-                try:
-                    _thread.start_new_thread(op_controller.PID_controller, input_for_controller)
-                except:
-                    print("Error: unable to start thread")
-                if not data:
-                    break
-                # conn.sendall(data)
+            try:
+                print('Connected by', addr)
+
+                data_from_logical_layer = conn.recv(1024)
+                input_for_controller = (data_from_logical_layer, n)
+                # print(input_for_controller)
+                threads.append(_thread.start_new_thread(op_controller.PID_controller, input_for_controller))
+                n = n + 1
+
+            except (KeyboardInterrupt, SystemExit, Exception):
+                for thread in threads:
+                    _thread.exit()
+                    print("Stopped Thread {0}".format(thread))
+                conn.close()
+                print("Now has stopped")
+                sys.exit()
     conn.close()
+
+# one error is the change of port
+
+
+
+
+
+
+
+# where actuator is specified
+# defined use case with identificative name for look up table
+# shall it be a text config file?
