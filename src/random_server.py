1070a1a8314c467b289b3cf3fc147fa43911e145
Federico Zarelli <Federico>
2018-05-07 11:46:38 +0200

    introduced class for reading sensors

--- src/random_server.py
+++ src/random_server.py
@@ -10,3 +10,11 @@ class random_server(object):
         for valve in valves.keys():
             setpoint = random.randint(min_operating, max_operating)
             valves[valve].set_status(setpoint)
+
+
+class read_current_status(object):
+    def __init__(self, pumps, sensors, valves):
+        # INTERFACE = SWITCHBOARD_PYTHON_API()
+        for valve in valves.keys():
+            setpoint = 1  # INTERFACE.GET_VALVE_POSITION(VALVE_NAME)
+            valves[valve].set_status(setpoint)
