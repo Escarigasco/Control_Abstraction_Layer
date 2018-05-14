23be226806794b64632435ba41748ca437b97ab0
Federico Zarelli <Federico>
2018-05-14 17:37:59 +0200

    moduled output signal according to percentage

--- src/controller.py
+++ src/controller.py
@@ -16,13 +16,13 @@ class controller(object):
         value = 0
         return value
 
-    def PID_controller(self, inputs, thread_ID):
+    def PID_controller(self, inputs, process_ID):
 
-        print("Control Process {0} started".format(thread_ID))
-        print("I am process", thread_ID)
+        print("Control Process {0} started".format(process_ID))
+        print("I am process", process_ID)
         inputs = pickle.loads(inputs)
         print(inputs)
-        self.thread_ID = thread_ID
+        self.process_ID = process_ID
         max = 100
         min = 0
         error = []
@@ -39,7 +39,7 @@ class controller(object):
         pre_error = 0
         windup_corrector = 0
         actuator_signal = 0
-        interface = syslab.HeatSwitchBoard(_BUILDING_NAME)
+        #interface = syslab.HeatSwitchBoard(_BUILDING_NAME)
         shut_down_signal = 0
         # interface.setPumpMode(circulators[n], circulator_mode[n])
         # for n in len(circulators):
@@ -47,14 +47,12 @@ class controller(object):
 
         while(1):
             try:
-                print("Control Thread {0} running".format(thread_ID))
+                print("Control Thread {0} running".format(process_ID))
                 time.sleep(_CONTROL_TIME)
-                feedback_value = interface.getThermalPower(feedback_sensor).value
-                print(feedback_value)
-                print(interface.getThermalPower(feedback_sensor))
-                #feedback_value = 5  # interface.getThermalPower(feedback)                    # Get the feedback value
-                                        # Get the set value
-
+                #feedback_value = interface.getThermalPower(feedback_sensor).value
+                #print(feedback_value)
+                #print(interface.getThermalPower(feedback_sensor))
+                feedback_value = 0
                 error_value = gain * (setpoint - feedback_value)       # Calculate the error
                 integral = (integral + error_value) - windup_corrector              # Calculate integral
                 derivative = error_value - pre_error           # Calculate derivative
@@ -63,23 +61,23 @@ class controller(object):
 
                 windup_corrector = self.controller_wind_up(actuator_signal, controller_output, ki)
 
-                if (controller_output > max):
+                controller_output_percentage = self.pump_setpoint_converter(controller_output)
+
+                if (controller_output_percentage > max):
                     actuator_signal = 100  # Limit the controller_output to maximum 255.
-                elif (controller_output < min):
+                elif (controller_output_percentage < min):
                     actuator_signal = 1
                 else:
-                    actuator_signal = controller_output
+                    actuator_signal = controller_output_percentage
                 print(actuator_signal)
                 # interface.setPumpSetpoint(circulators, actuator_signal)
                 error.append(error_value)
                 time_response.append(feedback_value)  # Save as previous error.
                 signal.signal(signal.SIGTERM, self.signal_term_handler)
-            except (KeyboardInterrupt, SystemExit, Exception):
-
-                # interface.interface.setPumpSetpoint(circulators, shut_down_signal)
+            except (KeyboardInterrupt, Exception, SystemExit):
+                # interface.setPumpSetpoint(circulators, actuator_signal)
                 print("Circulators is now at zero flow")
-                print("Process Error. Stopped")
-                sys.exit()
+                sys.exit(0)
 
     def controller_wind_up(self, actuator_signal, controller_output, ki):
         error_saturation = actuator_signal - controller_output
@@ -87,11 +85,20 @@ class controller(object):
         return windup_corrector
 
     def signal_term_handler(signal, frame):
-        print('got SIGTERM')
-        sys.exit(0)
+        print("It has to be here to receive signal but doesn't effectively do anything this is not going to be printed but it handles the asynchronous interaction")
+        #print('got SIGTERM - the process was killed as the configuration was not matched any more')
+        #print("Circulators is now at zero flow")
+        # interface.setPumpSetpoint(circulators, actuator_signal)
+        #sys.exit(0)
+
+    def pump_setpoint_converter(self, volume_flow):
+        pump_max_volume_flow = 10
+        volume_flow_in_percent = 100 * (volume_flow / pump_max_volume_flow)
+        return volume_flow_in_percent
+
 
 if __name__ == "__main__":
     test = controller()
-    input_for_controller = {"gain": 1, "kp": 2.58, "ki": 2.58, "kd": 0, "circulator": "Pump", "circulator_mode": "constant m", "actuator": "pump", "setpoint": 50, "feedback": 'sensors'}
+    input_for_controller = {"gain": 1, "kp": 2.58, "ki": 2.58, "kd": 0, "circulator": "Pump", "circulator_mode": "constant m", "actuator": "pump", "setpoint": 50, "feedback_sensor": 'sensors'}
     inputs = pickle.dumps(input_for_controller)
     test.PID_controller(inputs, "test_controller")
