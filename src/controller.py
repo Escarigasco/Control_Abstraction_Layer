d7ae6dbdf35a346670f61ba1860fd9c8589c1cd0
Federico Zarelli <Federico>
2018-05-14 18:11:38 +0200

    added composite measurements creation

--- src/controller.py
+++ src/controller.py
@@ -2,13 +2,14 @@
 # remember that additional gain can be added by multiplying the error by a constant
 # you could initialize this controller as a class when you initialize the logical layer and then call the method that start the thread every time you need it
 import pickle
+import signal
 import sys
 import syslab
+import syslab.core.datatypes.CompositeMeasurement as CM
 import time
-import signal
 _BUILDING_NAME = "716-h1"
 _CONTROL_TIME = 2
-
+_MULTIPLIER = 1000000
 
 class controller(object):
 
@@ -17,6 +18,7 @@ class controller(object):
         return value
 
     def PID_controller(self, inputs, process_ID):
+
 
         print("Control Process {0} started".format(process_ID))
         print("I am process", process_ID)
@@ -39,7 +41,7 @@ class controller(object):
         pre_error = 0
         windup_corrector = 0
         actuator_signal = 0
-        #interface = syslab.HeatSwitchBoard(_BUILDING_NAME)
+        interface = syslab.HeatSwitchBoard(_BUILDING_NAME)
         shut_down_signal = 0
         # interface.setPumpMode(circulators[n], circulator_mode[n])
         # for n in len(circulators):
@@ -49,9 +51,10 @@ class controller(object):
             try:
                 print("Control Thread {0} running".format(process_ID))
                 time.sleep(_CONTROL_TIME)
-                #feedback_value = interface.getThermalPower(feedback_sensor).value
+                feedback_value = interface.getThermalPower(feedback_sensor).value
+
                 #print(feedback_value)
-                #print(interface.getThermalPower(feedback_sensor))
+                print(interface.getThermalPower(feedback_sensor))
                 feedback_value = 0
                 error_value = gain * (setpoint - feedback_value)       # Calculate the error
                 integral = (integral + error_value) - windup_corrector              # Calculate integral
@@ -70,12 +73,15 @@ class controller(object):
                 else:
                     actuator_signal = controller_output_percentage
                 print(actuator_signal)
-                # interface.setPumpSetpoint(circulators, actuator_signal)
+                CompositMess = CM(actuator_signal, time.time() * _MULTIPLIER)
+                print(CompositMess)
+                # interface.setPumpSetpoint(circulators, CompositMess)
                 error.append(error_value)
                 time_response.append(feedback_value)  # Save as previous error.
                 signal.signal(signal.SIGTERM, self.signal_term_handler)
             except (KeyboardInterrupt, Exception, SystemExit):
-                # interface.setPumpSetpoint(circulators, actuator_signal)
+                CompositMess = CM(actuator_signal, time.time() * _MULTIPLIER)
+                # interface.setPumpSetpoint(circulators, CompositMess)
                 print("Circulators is now at zero flow")
                 sys.exit(0)
 
@@ -99,6 +105,6 @@ class controller(object):
 
 if __name__ == "__main__":
     test = controller()
-    input_for_controller = {"gain": 1, "kp": 2.58, "ki": 2.58, "kd": 0, "circulator": "Pump", "circulator_mode": "constant m", "actuator": "pump", "setpoint": 50, "feedback_sensor": 'sensors'}
+    input_for_controller = {"gain": 1, "kp": 2.58, "ki": 2.58, "kd": 0, "circulator": "Pump", "circulator_mode": "constant m", "actuator": "pump", "setpoint": 50, "feedback_sensor": 'Bay_4'}
     inputs = pickle.dumps(input_for_controller)
     test.PID_controller(inputs, "test_controller")
