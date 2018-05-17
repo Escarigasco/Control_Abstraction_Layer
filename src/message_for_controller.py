62b99a2c046cbd095519b4dc3c9aec888d605e49
Federico Zarelli <Federico>
2018-05-17 14:51:36 +0200

    added translator and removed references to syslab

--- src/message_for_controller.py
+++ src/message_for_controller.py
@@ -5,11 +5,10 @@ import pickle
 from rule_engine import rule_engine
 import socket
 import sys
-import syslab
+
 import time
 _ACTIVE = 1
 _INACTIVE = 0
-_BUILDING_NAME = "716-h1"
 _FIRST_OF_CLASS = 1
 
 
@@ -32,15 +31,14 @@ class message_for_controller(object):
             config = configparser.ConfigParser()
             config.read("/home/federico/Desktop/SwitchBoard/SwitchBoard/src/config_controller.txt")
 
-            #interface_syslab = syslab.HeatSwitchBoard(_BUILDING_NAME)
-            interface_syslab = 0
+
 
             nodes = list(unique.nodes)
             for node in nodes:
                 unique_nodes[node] = system_components[node]
 
             c_status = components_status()
-            available_components = c_status.run(interface, interface_syslab, unique_nodes)
+            available_components = c_status.run(interface, unique_nodes)
             engine = rule_engine()
             ideal_components = engine.run(system_input, available_components)
             act_circulator = self.pump_selector(ideal_components["Ideal_Pump"], available_components["Pumps_active"])
@@ -49,17 +47,21 @@ class message_for_controller(object):
             print(actuators)
             feedback_sensors = self.sensor_selector(ideal_components["Ideal_Sensor"], available_components["Sensors_active"], system_input["parameters"])
             print(feedback_sensors)
-            feedback_sensors = self.name_translator(feedback_sensors["sensors"])
-            controller_name = act_circulator["mode"]
+            act_circulator["pumps"] = self.circulator_name_translator(act_circulator["pumps"])
+            print(act_circulator)
+            feedback_sensors["sensors"] = self.sensors_name_translator(feedback_sensors["sensors"])
+            print(feedback_sensors)
+            actuators["actuators"] = self.actuators_name_translator(actuators["actuators"])
+            print(actuators)
+            self.controller_name = act_circulator["mode"]
 
-
-            input_for_controller = {"controller_name": controller_name, "kill": 'N', "gain": config.get(controller_name, "gain"), "kp": config.get(controller_name, "kp"),
-                                    "ki": config.get(controller_name, "ki"), "kd": config.get(controller_name, "kd"),
+            input_for_controller = {"controller_name": self.controller_name, "kill": 'N', "gain": config.get(self.controller_name, "gain"), "kp": config.get(self.controller_name, "kp"),
+                                    "ki": config.get(self.controller_name, "ki"), "kd": config.get(self.controller_name, "kd"),
                                     "circulator": act_circulator["pumps"], "circulator_mode": act_circulator["mode"],
-                                    "actuator": actuators, "setpoint": system_input['setpoints'], "feedback_sensor": feedback_sensors}
+                                    "actuator": actuators["actuators"], "setpoint": system_input['setpoints'], "feedback_sensor": feedback_sensors["sensors"]}
             print(input_for_controller)
-            sys.exit()
-            '''
+            #sys.exit()
+
             try:
                 if ((len(system_input["sinks"]) == 1) & (len(system_input["sources"]) == 1) & (system_input["boosted"] == 'N')):
                     message_serialized = pickle.dumps(input_for_controller)
@@ -71,10 +73,10 @@ class message_for_controller(object):
                     s.close()
             except Exception:
                 print("Message sending failed")
-                pass'''
+                pass
 
         def kill(self, system_input):
-            input_for_controller = {"controller_name": "Constant_Energy_Pump_Actuating", "kill": 'Y'}
+            input_for_controller = {"controller_name": self.controller_name, "kill": 'Y'}
             print(input_for_controller)
             try:
                 if ((len(system_input["sinks"]) == 1) & (len(system_input["sources"]) == 1) & (system_input["boosted"] == 'N')):
@@ -95,7 +97,7 @@ class message_for_controller(object):
 #       - if two sinks always use both pumps of each sink
 # sensor -
 
-        def name_translator(self, sensors):
+        def sensors_name_translator(self, sensors):
             translated_sensors = []
             sensors_name = {
                 'Sensor_1HT4': "Bay_4", 'Sensor_1CT4': "Bay_4", 'Sensor_1CF4': "Bay_4", 'Sensor_1E4': "Bay_4",
@@ -106,6 +108,38 @@ class message_for_controller(object):
             for sensor in sensors:
                 translated_sensors.append(sensors_name[sensor])
             return translated_sensors
+
+        def actuators_name_translator(self, actuators):
+            translated_actuator = []
+            actuator_name = {
+                'Pump_1C4': "Pump_Bay4",
+                'Pump_1H5': "Pump_Bay5",
+                'Pump_1H6': "Pump_Bay6",
+                'Pump_1H7': "Pump_Bay7",
+                'Pump_1H8': "Pump_Bay8",
+                'Valve_2C4': "Bay_4L-Busbar_2R", 'Valve_1C4': "Bay_4L-Busbar_1R", 'Valve_1B4': "Bay_4H-Busbar_B", 'Valve_2H4': "Bay_4H-Busbar_2F", 'Valve_1H4': "Bay_4H-Busbar_1F", 'Valve_2B4': "Bay_4L-Busbar_B",
+                'Valve_2C5': "Bay_5L-Busbar_1R", 'Valve_1C5': "Bay_5L-Busbar_2R", 'Valve_1B5': "Bay_5H-Busbar_B", 'Valve_2H5': "Bay_5H-Busbar_1F", 'Valve_1H5': "Bay_5H-Busbar_2F", 'Valve_2B5': "Bay_5L-Busbar_B",
+                'Valve_2C6': "Bay_6L-Busbar_1R", 'Valve_1C6': "Bay_6L-Busbar_2R", 'Valve_1B6': "Bay_6H-Busbar_B", 'Valve_2H6': "Bay_6H-Busbar_1F", 'Valve_1H6': "Bay_6H-Busbar_2F", 'Valve_2B6': "Bay_6L-Busbar_B",
+                'Valve_2H7': "Bay_7H-Busbar_1F", 'Valve_1H7': "Bay_7H-Busbar_2F", 'Valve_2C7': "Bay_7L-Busbar_1R", 'Valve_1C7': "Bay_7L-Busbar_2R",
+                'Valve_2H8': "Bay_8H-Busbar_1F", 'Valve_1H8': "Bay_8H-Busbar_2F", 'Valve_2C8': "Bay_8L-Busbar_1R", 'Valve_1C8': "Bay_8L-Busbar_2R"}
+            for actuator in actuators:
+                translated_actuator.append(actuator_name[actuator])
+            return translated_actuator
+
+        def circulator_name_translator(self, circulators):
+            translated_circulator = []
+            circulators_name = {
+                'Pump_1C4': "Pump_Bay4",
+                'Pump_1H5': "Pump_Bay5",
+                'Pump_1H6': "Pump_Bay6",
+                'Pump_1H7': "Pump_Bay7",
+                'Pump_1H8': "Pump_Bay8"}
+            for circulator in circulators:
+                translated_circulator.append(circulators_name[circulator])
+            return translated_circulator
+
+
+
 
         def pump_selector(self, ideal_pump, pumps):
             circulation_pumps = []
