185da33efc112bc04085776e117e079fa93c175e
Federico Zarelli <Federico>
2018-05-29 13:32:02 +0200

    class to select best configuration

---
+++ src/configuration_selector.py
@@ -0,0 +1,9 @@
+class configuration_selector(object):
+    def __init__(self, sensors, valves, pumps):
+        self.sensors = sensors
+        self.valves = valves
+        self.pumps = pumps
+
+    def start_screening(self, actuable_configuration):
+
+        
